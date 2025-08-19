dfe57a05b7bbab25aa26a03e71d33ee20eba069f
SantiagoSanchez20 <santisanchezg@javerianacali.edu.co>
2025-08-19 11:30:18 -0500

    Subida de la calculadora

---
+++ matematicas.cpp
@@ -0,0 +1,25 @@
+//
+// Created by duque on 19/08/2025.
+//
+#include <iostream>
+#include "matematicas.h"
+
+    // --- Definiciones (el "como") ---
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int res(int a, int b) {
+    return a - b;
+}
+
+int multi(int a, int b)
+{
+    return a * b;
+}
+
+double div(double a , double b)
+{
+    return a/b;
+}
